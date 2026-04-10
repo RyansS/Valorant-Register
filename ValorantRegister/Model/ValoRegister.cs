bcaeabf81618b6bea9f521193e91b47ba235ca80
RyansS <monteiroryan2016@gmail.com>
2026-04-10 15:54:39 -0300

    Created Model

--- ValorantRegister/Model/ValoRegister.cs
+++ ValorantRegister/Model/ValoRegister.cs
@@ -1,7 +1,24 @@
-﻿namespace ValorantRegister.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValorantRegister.Model
 {
     public class ValoRegister
     {
-        
+        [Key]
+        [Required]
+        public int Id { get; set;  }
+
+        [Required]
+        [MaxLength(16)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public string UserTag { get; set; }
+
     }
 }
